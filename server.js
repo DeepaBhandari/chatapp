e7b698d65e96fd64740a6f257eb43805b6ec86a3
deepab <me@deepab.com.np>
2024-02-23 10:46:14 +0545

    fix server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 
-app.use(express.static(path.join(_dirname+'/public')));
+app.use(express.static(path.join(__dirname+'/public')));
 
 server.listen(5000);
