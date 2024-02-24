039ec931ca01b10edf862be29005cafd25d5a0d6
deepab <me@deepab.com.np>
2024-02-24 10:24:55 +0545

    fix div css in chat

--- public/code.js
+++ public/code.js
@@ -1,17 +1,78 @@
-(function(){
+(function () {
     const app = document.querySelector(".app");
     const socket = io();
 
     let uname;
 
-    app.querySelector(".join-screen #join-user").addEventListener("click",function(){
-        let username = app.querySelector(".join-screen #username").ariaValueMax;
-        if(username.length == 0){
+    app.querySelector(".join-screen #join-user").addEventListener("click", () => {
+        let username = app.querySelector(".join-screen #username")?.value;
+        if (username?.length == 0) {
             return;
         }
         socket.emit("newuser", username);
         uname = username;
         app.querySelector(".join-screen").classList.remove("active");
-        app.querySelector(".chat-room").classList.add("active");
+        app.querySelector(".chat-screen ").classList.add("active");
+    });
+    app.querySelector(".chat-screen #send-message").addEventListener("click", () => {
+        let message = app.querySelector(".chat-screen #message-input")?.value;
+        if (message?.length == 0) {
+            return;
+        }
+        renderMessage("my", {
+            username: uname,
+            text: message
+        });
+        socket.emit("chat", {
+            username: uname,
+            text: message
+        })
+        app.querySelector(".chat-screen #message-input").value = "";
+    });
+
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", () => {
+        socket.emit("exituser", uname);
+        window.location.href = window.location.href;
+    });
+
+    socket.on("update", (update) => {
+        renderMessage("update", update);
+    });
+    socket.on("chat", (message) => {
+        renderMessage("other", message);
     })
+    const renderMessage = (type, message) => {
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if (type == "my") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "message my-message");
+            el.innerHTML =
+                ` <div>
+            <div class="name">
+               You
+            </div>
+            <div class="text">${message.text}</div>  
+        </div>`;
+            messageContainer.appendChild(el);
+        } else if (type == "other") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "message other-message");
+            el.innerHTML =
+                ` <div>
+            <div class="name">
+                ${message.username}
+            </div>
+            <div class="text">${message.text}</div>  
+        </div>`
+                ;
+            messageContainer.appendChild(el);
+        } else if (type == "update") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "update");
+            el.innerText = message;
+            messageContainer.appendChild(el);
+        }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+
 })()
--- server.js
+++ server.js
@@ -4,19 +4,24 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 
-app.use(express.static(path.resolve('./public')));
-const { Server } = require('socket.io');
-const io = new Server(server);
+// const { Server } = require('socket.io');
+// const io = new Server(server);
+const io = require("socket.io")(server);
 
-io.on("connection", function (socket) {
-    socket.on("newuser", function (username) {
+// app.use(express.static(path.resolve('./public')));
+app.use(express.static(path.join(__dirname + '/public')));
+
+io.on("connection", (socket) => {
+    socket.on("newuser", (username) => {
         socket.broadcast.emit("update", `${username} joined the conversation`);
     });
-    socket.on("exituser", function (username) {
+    socket.on("exituser", (username) => {
         socket.broadcast.emit("update", `${username} left the conversation`);
     });
     socket.on("chat", function (message) {
         socket.broadcast.emit("chat", message);
     });
 });
+
+
 server.listen(5000, () => console.log('server started at 5000 port'));
